cb6d6f6955e6f035ae48ce8455ec15de03acc683
asyl <asilbekmubarakov@mail.ru>
2019-01-21 06:14:20 +0600

    slider complete

--- frontend/theme/assets/js/script.js
+++ frontend/theme/assets/js/script.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-            // navText: ["<div class='slider-item_nav'><div class='slider-item-nav_arrow slider-item-nav_arrow--left'></div></div>",
-            //     "<div class='slider-item_nav'><div class='slider-item-nav_arrow slider-item-nav_arrow--right'></div></div>"],
-            loop: false,
+            navText: ["",""],
+            loop: true,
             margin: 0,
             nav: true,
             items: 1,
