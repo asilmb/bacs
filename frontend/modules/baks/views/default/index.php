04ac5640483878e5bd7f1e9909b9c56fd1b06bcf
amubarak <amubarak@wooppay.com>
2019-01-25 14:18:32 +0600

    add html

--- frontend/modules/baks/views/default/index.php
+++ frontend/modules/baks/views/default/index.php
@@ -52,9 +52,17 @@ $this->title = t('account/main', 'title');
             </div>
             <ul class="catalog-present_collections catalog-present-collections">
                 <li class="catalog-present-collections_selector girl"><a href="#!">Женская коллекция</a></li>
-                <li class="catalog-present-collections_selector men"><a href="#!">мужская коллекция</a></li>
+                <li class="catalog-present-collections_selector man"><a href="#!">мужская коллекция</a></li>
                 <li class="catalog-present-collections_selector travel"><a href="#!">путешествия</a></li>
             </ul>
+            <div class="catalog-present_comment">
+                <p>
+                    Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является
+                    стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный
+                    печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки
+                    образцов.
+                </p>
+            </div>
         </div>
         <div class="catalog-present_area catalog-present_area--right catalog-present-collections">
             <img src="images/collections/girl_collection.png" class="catalog-present-collections_item girl active">
@@ -68,23 +76,25 @@ $this->title = t('account/main', 'title');
     <h2 class="section-stock_title">Акции</h2>
     <div class="owl-carousel stock-carousel">
         <a class="owl-carousel__item stock-carousel_item stock-carousel_item--full ">
-            <img class="stock-carousel_item_img" src="/images/main-present-background.png" alt="">
+            <img class="stock-carousel_item_img" src="/images/stock/temp2.png" alt="">
         </a>
         <a class="owl-carousel__item stock-carousel_item stock-carousel_item--info ">
             <img class="stock-carousel_item_img" src="/images/main-present-background.png" alt="">
             <div class="stock-carousel_item_info stock-item">
                 <p class="stock-item_date">12. 05. 2019</p>
                 <h4 class="stock-item_title">Заголовок Акции</h4>
-                <p class="stock-item_describe">Новая коллекция! Kroco Из натуральной кожи.Сумка в дорогу или спорт зал также подходит для переноски, бизнес клатч и портмоне</p>
+                <p class="stock-item_describe">Новая коллекция! Kroco Из натуральной кожи.Сумка в дорогу или спорт
+                    зал также подходит для переноски, бизнес клатч и портмоне</p>
                 <div class="stock-item_more-btn"></div>
             </div>
         </a>
         <a class="owl-carousel__item stock-carousel_item stock-carousel_item--info">
-            <img class="stock-carousel_item_img" src="/images/main-present-background.png" alt="">
+            <img class="stock-carousel_item_img" src="/images/stock/temp2.png" alt="">
             <div class="stock-carousel_item_info stock-item">
                 <p class="stock-item_date">12. 05. 2019</p>
                 <h4 class="stock-item_title">Заголовок Акции</h4>
-                <p class="stock-item_describe">Новая коллекция! Kroco Из натуральной кожи.Сумка в дорогу или спорт зал также подходит для переноски, бизнес клатч и портмоне</p>
+                <p class="stock-item_describe">Новая коллекция! Kroco Из натуральной кожи.Сумка в дорогу или спорт
+                    зал также подходит для переноски, бизнес клатч и портмоне</p>
                 <div class="stock-item_more-btn"></div>
             </div>
         </a>
@@ -123,19 +133,104 @@ $this->title = t('account/main', 'title');
                 Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х
                 годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах
                 которых
-                используется Lorem IpsumLorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
-                Lorem
-                Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий
-                безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для
-                распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и
-                перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов
-                Letraset с
-                образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа
-                Aldus
-                PageMaker, в шаблонах которых используется Lorem Ipsum
+
             </p>
         </div>
         <img class="section-about_area-photo" src="/images/about-photo.png" alt="">
     </div>
-</div>
+    <div class="section-about_map map">
+        <div class="map-contacts">
+            <h4 class="map-contacts_title">Контакты</h4>
+            <div class="map-contacts_info contacts">
+                <div class="contacts_info">
+                    <p class="contacts_info-item"> +7 (777) 252-77-52</p>
+                    <p class="contacts_info-item"> +7 (777) 252-77-52</p>
+                    <p class="contacts_info-item"> Pochta@mail.ru</p>
+                </div>
+                <div class="contacts_city">
+                    <p class="contacts_city-name">г.Астана:</p>
+                    <div class="contacts_city-info-table">
+                        <div class="dots">
+                                    <span class="field">ТРЦ «Мега», Кургальджинское шоссе 1,
+2 этаж</span>
+                        </div>
+                        <span class="field">тел +7(7172)79-09-49</span>
 
+                    </div>
+                </div>
+                <div class="contacts_city">
+                    <p class="contacts_city-name">г.Астана:</p>
+                    <div class="contacts_city-info-table">
+                        <div class="dots">
+                                    <span class="field">ТРЦ «Мега», Кургальджинское шоссе 1,
+2 этаж</span>
+                        </div>
+                        <span class="field">тел +7(7172)79-09-49</span>
+
+                    </div>
+                    <div class="contacts_city-info-table">
+                        <div class="dots">
+                                    <span class="field">ТРЦ «Мега», Кургальджинское шоссе 1,
+2 этаж</span>
+                        </div>
+                        <span class="field">тел +7(7172)79-09-49</span>
+
+                    </div>
+                    <div class="contacts_city-info-table">
+                        <div class="dots">
+                                    <span class="field">ТРЦ «Мега», Кургальджинское шоссе 1,
+2 этаж</span>
+                        </div>
+                        <span class="field">тел +7(7172)79-09-49</span>
+
+                    </div>
+                    <div class="contacts_city-info-table">
+                        <div class="dots">
+                                    <span class="field">ТРЦ «Мега», Кургальджинское шоссе 1,
+2 этаж</span>
+                        </div>
+                        <span class="field">тел +7(7172)79-09-49</span>
+
+                    </div>
+                    <div class="contacts_city-info-table">
+                        <div class="dots">
+                                    <span class="field">ТРЦ «Мега», Кургальджинское шоссе 1,
+2 этаж</span>
+                        </div>
+                        <span class="field">тел +7(7172)79-09-49</span>
+
+                    </div>
+                </div>
+                <div class="contacts_city">
+                    <p class="contacts_city-name">г.Астана:</p>
+                    <div class="contacts_city-info-table">
+                        <div class="dots">
+                                    <span class="field">ТРЦ «Мега», Кургальджинское шоссе 1,
+2 этаж</span>
+                        </div>
+                        <span class="field">тел +7(7172)79-09-49</span>
+
+                    </div>
+                </div>
+                <div class="contacts_add-info">
+                    <p>Жалобы и Предложения принимаются на Bags_accessorize@mail.ru</p>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="map-geo">
+            <div class="map-geo_cities geo-cities">
+                <p class="geo-cities_city astana active">Астана</p>
+                <p class="geo-cities_city karagandy">Караганда</p>
+            </div>
+            <div class="map-geo_yandex astana active">
+                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A17c948431434ecbbbd226801c0c59555f782932e7e704a2a444dc78dc85aab08&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true"></script>
+            </div>
+            <div class="map-geo_yandex karagandy">
+                <script type="text/javascript" charset="utf-8" async
+                        src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A5c65a68098e62c67b3dad5fc5ff02623a52e2e217c59c3dd8bd3f31aa66274f1&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true"></script>
+            </div>
+        </div>
+    </div>
+</div>
