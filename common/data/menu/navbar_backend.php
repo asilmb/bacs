7f06860f64bd3cb9a1c0dfe4b7991df50c4d03e6
asyl <asilbekmubarakov@mail.ru>
2019-01-27 03:13:19 +0600

    clear admin

--- common/data/menu/navbar_backend.php
+++ common/data/menu/navbar_backend.php
@@ -2,21 +2,21 @@
 
 return [
 	'mainMenu' => [
-//		[
-//			'label' => ['admin', 'main'],
-//		],
-//		'yii2module\article\admin\helpers\Menu',
-//		'yii2woop\service\admin\helpers\Menu',
-//		[
-//			'label' => ['admin', 'system'],
-//		],
-//		'yii2lab\applicationTemplate\backend\helpers\Menu',
-//		//'yii2lab\app\admin\helpers\Menu',
-//		'yii2lab\notify\admin\helpers\Menu',
-//		'yii2lab\rbac\admin\helpers\Menu',
-//		[
-//			'label' => ['admin', 'develop'],
-//		],
+		[
+			'label' => ['admin', 'main'],
+		],
+		'yii2module\article\admin\helpers\Menu',
+		'yii2woop\service\admin\helpers\Menu',
+		[
+			'label' => ['admin', 'system'],
+		],
+		'yii2lab\applicationTemplate\backend\helpers\Menu',
+		//'yii2lab\app\admin\helpers\Menu',
+		'yii2lab\notify\admin\helpers\Menu',
+		'yii2lab\rbac\admin\helpers\Menu',
+		[
+			'label' => ['admin', 'develop'],
+		],
 //		'yii2lab\applicationTemplate\backend\helpers\ToolsMenu',
 //		'yii2module\vendor\admin\helpers\Menu',
 	],
