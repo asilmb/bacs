1ba3204c8cffb00c86b1b299bff4a9ab96a9039a
asyl <asilbekmubarakov@mail.ru>
2019-02-03 19:11:09 +0600

    styles

--- frontend/modules/baks/views/default/collectionItem.php
+++ frontend/modules/baks/views/default/collectionItem.php
@@ -2,9 +2,9 @@
 /** @var \domain\v1\finance\entities\CollectionEntity $item */
 ?>
 <a class="owl-carousel__item catalog-view_item ">
-    <div class="catalog-view_item_img-wrapper">
-        <img src="/images/collections/<?= $item->collectionType . '/' . $item->image ?>" alt="">
-    </div>
+        <div class="catalog-view_item_img-wrapper">
+            <img src="/images/collections/<?= $item->collectionType . '/' . $item->image ?>" alt="">
+        </div>
 
     <div class="catalog-view_item_info view-info">
         <?php if(!empty($item->desc)): ?>
